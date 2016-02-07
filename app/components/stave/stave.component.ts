dba7ef90cdeafae366c44cb81ca1c3d4ad9f624c
Kohei Hisakuni <khisakuni@gmail.com>
2016-02-07 12:52:47 -0800

    ChangePitchService lowers and raises pitch of ntoes

--- app/components/stave/stave.component.ts
+++ app/components/stave/stave.component.ts
@@ -1,12 +1,14 @@
 import {Component, View, Inject, ViewChild} from 'angular2/core';
 import {RendererService} from '../../services/renderer.service';
+import {ChangePitchService} from '../../services/change-pitch.service';
 import {VoiceDecorator} from '../../models/voice.decorator';
 import * as _ from 'lodash';
 import './stave.style.scss';
 
 @Component({
   selector: 'stave',
-  inputs: ['stave']
+  inputs: ['stave'],
+  providers: [ChangePitchService]
 })
 @View({
   templateUrl: 'app/components/stave/stave.template.html'
@@ -20,11 +22,14 @@ export class StaveComponent {
   stave: Vex.Flow.Stave;
   canvasSelector: string;
   renderer: RendererService;
+  changePitchService: ChangePitchService;
   voice: VoiceDecorator;
   notes: Array<Vex.Flow.Note>;
   selectedNoteIndex: number;
 
   constructor() {
+    this.changePitchService = new ChangePitchService
+
     var notes = [
         new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "q" }),
         new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "q" }),
@@ -94,74 +99,18 @@ export class StaveComponent {
   }
 
   raisePitch() {
-    let selected = this.selectedNote();
-    let key = selected.getKeys()[0];
-    let newKey = this.raiseKey(key);
-
-    this.updateNote(newKey, 'q');
-    this.updateVoice(this.voice.vfNotes);
+    let updates = this.changePitchService.raisePitch(this.selectedNote(), this.voice.vfVoice);
+    this.voice.vfVoice = updates.voice;
+    this.voice.vfNotes = updates.voice.getTickables()
+    this.selectNote(updates.note);
+    this.renderer.drawVoice(this.stave, updates.voice);
   }
 
   lowerPitch() {
-    let selected = this.selectedNote();
-    let key = selected.getKeys()[0];
-    let newKey = this.lowerKey(key);
-
-    this.updateNote(newKey, 'q');
-    this.updateVoice(this.voice.vfNotes);
-  }
-
-  raiseKey(key: string) : string {
-    let [noteName, octave] = key.split('/');
-    let newNoteName = this.newNoteName(noteName.charCodeAt(0) + 1);
-    let raiseOctave = this.shouldRaiseOctave(noteName, newNoteName);
-    let newoctave = raiseOctave ? String(+octave + 1) : octave;
-    return `${newNoteName}/${newoctave}`;
-  }
-
-  lowerKey(key: string) : string {
-    let [noteName, octave] = key.split('/');
-    let newNoteName = this.newNoteName(noteName.charCodeAt(0) - 1);
-    let lowerOctave = this.shouldLowerOctave(noteName, newNoteName);
-    let newoctave = lowerOctave ? String(+octave - 1) : octave;
-    return `${newNoteName}/${newoctave}`
-  }
-
-  shouldRaiseOctave(noteName: string, newNoteName: string) : boolean {
-    return (noteName == 'b' && newNoteName == 'c')
-  }
-
-  shouldLowerOctave(noteName: string, newNoteName: string) : boolean {
-    return (noteName == 'c' && newNoteName == 'b')
-  }
-
-  newNoteName(charCode: number) : string {
-    let newCharCode;
-    if (charCode > 103) {
-      newCharCode = 97;
-    } else if (charCode < 97) {
-      newCharCode = 103;
-    } else {
-      newCharCode = charCode;
-    }
-
-    return String.fromCharCode(newCharCode);
-  }
-
-  updateNote(key: string, duration: string) {
-    let note = new Vex.Flow.StaveNote({ keys: [key], duration: duration })
-    this.voice.vfNotes[this.selectedNoteIndex] = note;
-    this.selectNote(note);
-  }
-
-  updateVoice(notes: Array<Vex.Flow.Note>) : Vex.Flow.Voice {
-    let voice = new Vex.Flow.Voice({
-      num_beats: 4,
-      beat_value: 4,
-      resolution: Vex.Flow.RESOLUTION
-    });
-    voice.addTickables(notes);
-    this.voice.vfVoice = voice;
-    this.renderer.drawVoice(this.stave, voice);
+    let updates = this.changePitchService.lowerPitch(this.selectedNote(), this.voice.vfVoice);
+    this.voice.vfVoice = updates.voice;
+    this.voice.vfNotes = updates.voice.getTickables()
+    this.selectNote(updates.note);
+    this.renderer.drawVoice(this.stave, updates.voice);
   }
 }
---
+++ app/services/change-pitch.service.ts
@@ -0,0 +1,81 @@
+import * as _ from 'lodash';
+
+export class ChangePitchService {
+
+  raisePitch(note: Vex.Flow.Note, voice: Vex.Flow.Voice) : Vex.Flow.Voice {
+    let key = note.getKeys()[0];
+    let newKey = this.raiseKey(key);
+    let index = _.indexOf(voice.getTickables(), note)
+    let note = this.updateNote(newKey, 'q');
+    
+    return { voice: this.updateVoice(voice, note, index), note: note };
+  }
+
+  lowerPitch(note: Vex.Flow.Note, voice: Vex.Flow.Voice) : Vex.Flow.Voice {
+    let key = note.getKeys()[0];
+    let newKey = this.lowerKey(key);
+    let index = _.indexOf(voice.getTickables(), note)
+
+    this.updateNote(newKey, 'q');
+    return this.updateVoice(voice, note, index);
+  }
+
+  private updateNote(key: string, duration: string) : Vex.Flow.StaveNote {
+    return new Vex.Flow.StaveNote({ keys: [key], duration: duration });
+  }
+
+  private updateVoice(
+    voice: Vex.Flow.Voice,
+    note: Vex.Flow.StaveNote,
+    index: number
+  ) : Vex.Flow.Voice {
+    let newVoice = new Vex.Flow.Voice({
+      num_beats: 4,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    });
+    let notes = voice.getTickables();
+    notes[index] = note;
+
+    return newVoice.addTickables(notes);
+  }
+
+  private raiseKey(key: string) : string {
+    let [noteName, octave] = key.split('/');
+    let newNoteName = this.newNoteName(noteName.charCodeAt(0) + 1);
+    let raiseOctave = this.shouldRaiseOctave(noteName, newNoteName);
+    let newoctave = raiseOctave ? String(+octave + 1) : octave;
+
+    return `${newNoteName}/${newoctave}`;
+  }
+
+  private lowerKey(key: string) : string {
+    let [noteName, octave] = key.split('/');
+    let newNoteName = this.newNoteName(noteName.charCodeAt(0) - 1);
+    let lowerOctave = this.shouldLowerOctave(noteName, newNoteName);
+    let newoctave = lowerOctave ? String(+octave - 1) : octave;
+
+    return `${newNoteName}/${newoctave}`
+  }
+
+  private shouldRaiseOctave(noteName: string, newNoteName: string) : boolean {
+    return (noteName == 'b' && newNoteName == 'c')
+  }
+
+  private shouldLowerOctave(noteName: string, newNoteName: string) : boolean {
+    return (noteName == 'c' && newNoteName == 'b')
+  }
+
+  private newNoteName(charCode: number) : string {
+    let newCharCode;
+    if (charCode > 103) {
+      newCharCode = 97;
+    } else if (charCode < 97) {
+      newCharCode = 103;
+    } else {
+      newCharCode = charCode;
+    }
+
+    return String.fromCharCode(newCharCode);
+  }
+}
